24f8af730b01a5a7297aa192726313dfa15f1cb6
John Galvin <96908304+Johnnyg63@users.noreply.github.com>
2024-05-19 10:20:00 +0100

    Added files for sound and Player Moventment

---
+++ PlayerMovement.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Player.h"
+#include "olcPixelGameEngine.h"
+
+class Player;
+
+class PlayerMovement
+{
+
+private:
+
+	void ManageSpaceKey(float fElapsedTime);
+	
+public:
+	PlayerMovement(olc::PixelGameEngine& pge, Player& player);
+	olc::PixelGameEngine& pge;
+	Player& player;
+
+
+	/// <summary>
+	/// Call this method to update the player poistion
+	/// </summary>
+	/// <param name="fElapsedTime">fElapsedTime</param>
+	void Update(float fElapsedTime);
+
+};
